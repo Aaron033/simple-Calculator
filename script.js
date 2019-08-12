cbc490e37840cb5d91b2c02abfd42bac1198a29b
Aaron <aaron.botello.h@gmail.com>
2019-08-11 20:56:15 -0500

    setting the path

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 class Calculator{
     // All the new istances will get the structure 
     constructor( previousOperandTextElement, currentOperandTextElement){
